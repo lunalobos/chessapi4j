2bb259a4b6d64ddb43c776238e6fa00e5bbbfc4e
lunalobos <100201945+lunalobos@users.noreply.github.com>
2023-08-21 22:54:42 -0300

    Fix for Handling PGN Databases with Multi-Line Moves
    A bug was fixed that prevented handling PGN databases in which the moves
    of each game were not all found on a single line.

--- chess-api/src/test/java/chessapi4j/PGNHandlerTest.java
+++ chess-api/src/test/java/chessapi4j/PGNHandlerTest.java
@@ -2,11 +2,7 @@ package chessapi4j;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
