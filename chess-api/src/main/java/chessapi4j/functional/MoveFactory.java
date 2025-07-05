832e5da38dc8735e2355e84c793f495f119484a7
lunalobos <100201945+lunalobos@users.noreply.github.com>
2025-07-05 13:23:32 -0300

    Bump version to 1.2.10, update javadoc plugin version, and change default log level to WARN

--- chess-api/src/main/java/chessapi4j/functional/MoveFactory.java
+++ chess-api/src/main/java/chessapi4j/functional/MoveFactory.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2025 Miguel Angel Luna Lobos
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://github.com/lunalobos/chessapi4j/blob/master/LICENSE
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package chessapi4j.functional;
 
 import java.time.OffsetDateTime;
@@ -12,6 +27,11 @@ import chessapi4j.Util;
 
 import static chessapi4j.Piece.*;
 
+/**
+ *
+ * @author lunalobos
+ * @since 1.2.10
+ */
 final class MoveFactory {
     private static final Logger logger = Factory.getLogger(MoveFactory.class);
     private static final Pattern MOVE_PATTERN = Pattern.compile(
@@ -158,5 +178,5 @@ final class MoveFactory {
         } else {
             throw MovementException.invalidString(move);
         }
-    };
+    }
 }
